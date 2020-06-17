fe51fe93d544c45c2ed91e496f7870355264303f
joje1283 <joje1283@gmail.com>
2020-06-18 00:17:02 +0900

    Added post table in post app.

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField(verbose_name='subject', max_length=50)
+    content = models.TextField(verbose_name='content')
+    modified = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
