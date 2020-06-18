fb0d5c8c23360edce0f12d1028542b2401119ef1
joje1283 <joje1283@gmail.com>
2020-06-19 00:09:26 +0900

    Added bootstrap blog template in project.

--- blog/settings.py
+++ blog/settings.py
@@ -122,5 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'accounts.user'
+
+# static 경로 지정
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')   # .gitignore에 .static_root디렉토리 추가
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # 정적파일 우선탐색
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from posts.models import Post
+
+
+def home_view(request):
+    return render(request, 'index.html')
+
+
+def post_view(request, pk):
+    # post = get_object_or_404(Post, pk)
+    post = Post.objects.filter(pk=pk).first()
+    return render(request, 'post.html', {
+        'post': post
+    })
