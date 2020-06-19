6db2a3d30aa855a879c168d06a2f5a434fb8a6d4
joje1283 <joje1283@gmail.com>
2020-06-19 22:22:23 +0900

    Revert "Added bootstrap blog template in project."
    This reverts commit fb0d5c8c

--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,3 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 
-from posts.models import Post
-
-
-def home_view(request):
-    return render(request, 'index.html')
-
-
-def post_view(request, pk):
-    # post = get_object_or_404(Post, pk)
-    post = Post.objects.filter(pk=pk).first()
-    return render(request, 'post.html', {
-        'post': post
-    })
+# Create your views here.
