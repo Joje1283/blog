c547fdc3f0318779a216320dbf825b475d4a6251
joje1283 <joje1283@gmail.com>
2020-06-24 23:57:13 +0900

    Added Tagset in post

--- posts/admin.py
+++ posts/admin.py
@@ -1,13 +1,20 @@
 from django_summernote.admin import SummernoteModelAdmin
 from django.contrib import admin
-from .models import Post, Contact
+from .forms import PostForm
+from .models import Post, Contact, Tag
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
+    form = PostForm
 
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
--- posts/models.py
+++ posts/models.py
@@ -8,11 +8,19 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     subject = models.CharField(verbose_name='subject', max_length=200)
     content = models.TextField(verbose_name='content')
+    tag_set = models.ManyToManyField('Tag', blank=True)
     modified = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.subject
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=140, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Contact(models.Model):
